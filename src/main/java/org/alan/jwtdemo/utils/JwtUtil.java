c42cbfd04dca38f6c4c6dcb3748e5a8f07176a5d
Alan <45555879+iAlan66@users.noreply.github.com>
2025-03-11 18:06:00 +0800

    feat: 配置文件动态设置 JWT 密钥和过期时间

--- src/main/java/org/alan/jwtdemo/utils/JwtUtil.java
+++ src/main/java/org/alan/jwtdemo/utils/JwtUtil.java
@@ -5,7 +5,9 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.annotation.PostConstruct;
 import org.alan.jwtdemo.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
@@ -14,28 +16,37 @@ import java.util.Map;
 
 @Component
 public class JwtUtil {
-    public final static String secret = "123456";
 
+    @Value("${jwt.expiration}")
+    private Integer expiration;
+    @Value("${jwt.secret}")
+    private String secret;
+    private static JwtUtil INS;
+
+    @PostConstruct
+    public void init() {
+        INS = this;
+    }
     //根据用户信息创建Jwt令牌
     public static String createJwt(User user) {
         Calendar calendar = Calendar.getInstance();
         Date now = calendar.getTime();
 
-        //过期时间为7天
-        calendar.add(Calendar.SECOND, 3600 * 24 * 7);
+        //过期时间
+        calendar.add(Calendar.SECOND, INS.expiration);
         return JWT.create()
                 //配置JWT自定义信息
                 .withClaim("id", user.getId())
                 .withClaim("username", user.getUsername())
                 .withExpiresAt(calendar.getTime())  //设置过期时间
                 .withIssuedAt(now)    //设置创建时间
-                .sign(Algorithm.HMAC256(secret));   //最终签名
+                .sign(Algorithm.HMAC256(INS.secret));   //最终签名
     }
 
     //根据Jwt验证并解析用户信息
     public static User resolveJwt(String token) {
         try {
-            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(secret)).build();
+            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(INS.secret)).build();
             DecodedJWT verify = jwtVerifier.verify(token);  //对JWT令牌进行验证，看看是否被修改
             Map<String, Claim> claims = verify.getClaims();  //获取令牌中内容
 
